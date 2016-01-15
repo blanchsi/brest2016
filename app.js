e2d0ad9290baa15fab6559b8599f63401118c01c
Antoine Souesme <Antoine Souesme>
2016-01-15 23:17:45 +0100

    pas grand chose

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var reload = require('reload');
+var http = require('http');
 
 
 
@@ -27,6 +29,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -41,6 +45,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+//
+var server = http.createServer(app)
+reload(server, app, 10, 10)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
